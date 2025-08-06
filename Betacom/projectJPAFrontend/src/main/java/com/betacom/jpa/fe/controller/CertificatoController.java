0f92e421b409f14a7b7e55cc401552848bbdd079
AlessandroVIV <alex01.vivaldi@gmail.com>
2025-08-06 12:03:45 +0200

    Commit aggiunta pagina Certificato

--- Betacom/projectJPAFrontend/src/main/java/com/betacom/jpa/fe/controller/CertificatoController.java
+++ Betacom/projectJPAFrontend/src/main/java/com/betacom/jpa/fe/controller/CertificatoController.java
@@ -1,11 +1,15 @@
 package com.betacom.jpa.fe.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.betacom.jpa.fe.dto.SocioDTO;
+import com.betacom.jpa.fe.response.ResponseObject;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -19,16 +23,24 @@ public class CertificatoController {
 	@GetMapping("listCertificato")
 	public ModelAndView listCertificato(@RequestParam Integer id) {
 		
-		log.debug("listCertificato id: " + id);
-		
-		ModelAndView mav = new ModelAndView("listCertificato");
-		
-		
-		
-		return mav;
-		
-		
-		
+	    log.debug("listCertificato id: " + id);
+
+	    ModelAndView mav = new ModelAndView("listCertificato");
+
+	    SocioDTO socio = clientWeb.get()
+	        .uri(uriBuilder -> uriBuilder
+	            .path("socio/getSocioById")
+	            .queryParam("id", id)
+	            .build())
+	        .retrieve()
+	        .bodyToMono(new ParameterizedTypeReference<ResponseObject<SocioDTO>>() {})
+	        .block()
+	        .getDati();
+
+	    mav.addObject("certificato", socio.getCertificato());
+	    
+	    return mav;
+	    
 	}
-	
+
 }
